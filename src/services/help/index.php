789cb943ba7860c13c1bf78993294d5f90e12c72
Tristan Rivoallan <tristan@rivoallan.net>
2014-02-13 18:15:49 +0100

    [service/help] compat w/ new input parameters

--- src/services/help/index.php
+++ src/services/help/index.php
@@ -5,21 +5,21 @@ require_once(__DIR__.'/../../lib.php');
 // Get services list
 $services = getServices(__DIR__.'/..');
 
-if (is_null($body)) {
+if (is_null($parameters)) {
     // If no service name is provided, list available services
     echo sprintf('SERVICES : %s', implode(', ', array_keys($services)));
 } else {
-    if (in_array($body, array_keys($services))) {
+    if (in_array($parameters, array_keys($services))) {
         // Existing service
-        if (isset($services[$body]['help'])) {
+        if (isset($services[$parameters]['help'])) {
             // Service provides help
-            require_once($services[$body]['help']);
+            require_once($services[$parameters]['help']);
         } else {
             // Service do not provide help
-            echo sprintf('NO HELP FOR SERVICE "%s"', $body);
+            echo sprintf('NO HELP FOR SERVICE "%s"', $parameters);
         }
     } else {
         // Unknown service
-        echo sprintf('UNKNOWN SERVICE "%s"', $body);
+        echo sprintf('UNKNOWN SERVICE "%s"', $parameters);
     }
 }
